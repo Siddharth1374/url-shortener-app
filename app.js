e0e760ad269873d43282b8077a397e09b6de8098
Siddharth Yadav <calculus78910@gmail.com>
2026-04-13 17:47:03 +0530

    Update CORS configuration in app.js
    Refactor CORS middleware to allow specific origins and methods.

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const cors = require("cors");  
+const cors = require("cors");
 
 const app = express();
+app.use(cors({
+  origin: function (origin, callback) {
+    if (
+      !origin ||
+      origin.endsWith(".vercel.app") ||
+      origin === "http://localhost:5173"
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
-app.use(cors());            
 app.use(express.json());
 
 const urlRoutes = require("./routes/urlRoutes");
-
 app.use("/", urlRoutes);
 
 module.exports = app;
